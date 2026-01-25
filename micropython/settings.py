4ed9ef8c2cadbd93f955cb67913ab57cbe80a0f9
kevinnutt83 <152747362+kevinnutt83@users.noreply.github.com>
2026-01-25 14:17:39 +0000

    Update firmware version to v2.06.2 and refresh checksums in manifest files

--- micropython/settings.py
+++ micropython/settings.py
@@ -41,7 +41,7 @@ UNIT_Name = "No Device Name"    # Human-friendly name (provisioned)
 NODE_TYPE = 'base'             # 'base','wifi', or 'remote'; base can host LoRa network & WiFi; remote uses LoRa primarily
 #NODE_TYPE = 'remote'          # Uncomment for remote role during flashing
 
-FIRMWARE_VERSION = "v2.06.1"   # Firmware version string
+FIRMWARE_VERSION = "v2.06.2"   # Firmware version string
 
  # WordPress Unit Connector API integration
 WORDPRESS_API_URL = ""   # Customer Unit Connector site for provisioned devices
