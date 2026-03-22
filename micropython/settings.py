4c4187211151b79cb6ab69f2b71d0e3547aae26e
kevinnutt83 <152747362+kevinnutt83@users.noreply.github.com>
2026-03-22 21:23:27 +0000

    Update firmware version to v2.01.0i in settings and version files

--- micropython/settings.py
+++ micropython/settings.py
@@ -41,7 +41,7 @@ FIELD_DATA_GZIP = True
 UNIT_ID = "None"
 UNIT_Name = "No Device Name"
 NODE_TYPE = 'base'
-FIRMWARE_VERSION = "v2.01.0h"
+FIRMWARE_VERSION = "v2.01.0i"
  
 WORDPRESS_API_URL = ""
 WORDPRESS_USERNAME = "agadmin"
